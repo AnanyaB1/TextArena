624d741b595c7c69817357802642aac491317e9b
Dylan Hillier <dylanslavinhillier@gmail.com>
2025-03-08 12:37:36 +0800

    adds script for us

---
+++ hackathon.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+
+import textarena as ta
+
+load_dotenv()
+
+# Initialize agents
+agents = {
+    0: ta.agents.AnthropicAgent(model_name="claude-3-7-sonnet-20250219"),
+    1: ta.agents.AnthropicAgent(model_name="claude-3-7-sonnet-20250219"),
+}
+
+# Initialize environment from subset and wrap it
+env = ta.make(env_id="SpellingBee-v0")
+env = ta.wrappers.LLMObservationWrapper(env=env)
+# Optional render wrapper
+env = ta.wrappers.SimpleRenderWrapper(
+    env=env,
+    player_names={0: "GPT-4o-mini", 1: "claude-3.5-haiku"},
+)
+
+env.reset(num_players=len(agents))
+done = False
+while not done:
+    player_id, observation = env.get_observation()
+    action = agents[player_id](observation)
+    done, info = env.step(action=action)
+rewards = env.close()
